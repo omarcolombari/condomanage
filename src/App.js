848b4f078d072bf546be2e0a65ff51c7fd03ca88
LuanDev <luanpbs98@outlook.com>
2022-03-17 11:19:50 -0300

    feature:header/drawer/dashboard.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import DashboardUser from "./Pages/DashboardUser";
 
 function App() {
   return (
     <ChakraProvider>
-      <DashboardUser/>
     </ChakraProvider>
   )
 }
