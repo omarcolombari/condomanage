04456ed2a7b4913f28959559e95b5df83324129a
LuanDev <luanpbs98@outlook.com>
2022-03-16 23:53:57 -0300

    feature:input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import CustomTheme from "./Style/Theme";
-import "@fontsource/poppins";
-import "@fontsource/roboto";
 
 function App() {
   return (
-    <ChakraProvider theme={CustomTheme}>
+    <ChakraProvider>
     </ChakraProvider>
   )
 }
