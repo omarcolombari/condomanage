e089aea6ab8651dcb5d781a55474e38fc981b8ef
Omar <omarucolombari@gmail.com>
2022-03-17 12:58:31 -0300

    feat: feito providers demands, finances e tenants com suas respectivas requisiçoes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
+
   return (
     <div className="App">
+      <ToastContainer />
     </div>
   );
 }
---
+++ src/Providers/Finances/index.js
@@ -0,0 +1,60 @@
+import { createContext, useState } from "react";
+import { toast } from "react-toastify";
+import api from "../../services/api";
+
+export const FinancesContext = createContext()
+
+export const FinancesProvider = ({ children }) => {
+
+    const [finances, setFinances] = useState([])
+
+    const showFinances = (token) => {
+        api.get('/finances', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then((res) => {
+            console.log(res)
+            setFinances(res.data)
+        })
+        .catch((err) => console.log(err))
+    }
+
+    const addFinance = (id, token, data) => {
+        api.post('/finances', {
+            ...data,
+            "userId": id
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then((res) => {
+            console.log(res)
+            toast.success('Finança adicionada com sucesso!')
+        })
+        .catch((err) => {
+            console.log(err)
+            toast.error('Ops! Algo deu errado')
+        })
+    }
+
+    const changeFinance = (token, data, financeId) => {
+        api.patch(`/finances/${financeId}`)
+        .then((res) => {
+            console.log(res)
+            toast.success('Dados alterados com sucesso!')
+        })
+        .catch((res) => {
+            console.log(res)
+            toast.error('Ops! Algo deu errado')
+        })
+    }
+
+    return (
+        <FinancesContext.Provider value={{ finances, showFinances, addFinance, changeFinance }} >
+            {children}
+        </FinancesContext.Provider>
+    )
+}
