933104fca51edead53878ad7042607fe0a44a550
LuanDev <luanpbs98@outlook.com>
2022-03-17 00:47:19 -0300

    feature:Modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import Modals from "./Components/Modals";
 
 function App() {
   return (
     <ChakraProvider>
+      <Modals/>
     </ChakraProvider>
   )
 }
