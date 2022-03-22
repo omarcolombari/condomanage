539bbe389bae67e4711537e594cb819f02a1f4cc
Brendo Sousa <brendo.n.s.sousa@gmail.com>
2022-03-21 23:07:36 -0300

    build: Adição de efeitos na estilização dos cards

--- src/Pages/Finance/index.jsx
+++ src/Pages/Finance/index.jsx
@@ -9,18 +9,22 @@ import FinanceCard from "../../Components/FinanceCard";
 import { IoMdCash } from "react-icons/io";
 
 const Finance = () => {
-  const { onOpen:onContainerOpen } = useDisclosure();
-  const { isOpen:isAddFinanceOpen, onOpen:onAddFinanceOpen, onClose:onAddFinanceClose } = useDisclosure();
-  const [ filterFin,setFilterFin ] = useState("Todos");
+  const { onOpen: onContainerOpen } = useDisclosure();
+  const {
+    isOpen: isAddFinanceOpen,
+    onOpen: onAddFinanceOpen,
+    onClose: onAddFinanceClose,
+  } = useDisclosure();
+  const [filterFin, setFilterFin] = useState("Todos");
 
   //const token = JSON.parse(localStorage.getItem("@CondoManage:token"));
   const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RlQHRlc3RlLmNvbSIsImlhdCI6MTY0NzkwMzM0NywiZXhwIjoxNjQ3OTA2OTQ3LCJzdWIiOiIyIn0.0zjC3uCJPk50llrwIvFp94VpkBrUcTT26PWmKvhBMB8";
-  
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJyZW5kb0BlbWFpbC5jb20iLCJpYXQiOjE2NDc5MDYzMTUsImV4cCI6MTY0NzkwOTkxNSwic3ViIjoiMSJ9.vQk2_VP1J78-8vmJZpEPNYlqa8-p5u-oL7-nRJbz0qg";
+
   //Pegar o Id do usuário
   //const user = JSON.parse(localStorage.getItem("@CondoManage:infos"));
   //const userId = user.id
-  const userId = 2;
+  const userId = 1;
 
   //Pegando o array e os métodos do Providers
   const { finances, showFinances, addFinance } = useContext(FinancesContext);
@@ -29,7 +33,7 @@ const Finance = () => {
   const [newFinances, setNewFinances] = useState([...finances]);
 
   const loadFinances = async () => {
-    await showFinances(token,userId);
+    await showFinances(token, userId);
   };
 
   useEffect(() => {
@@ -45,93 +49,112 @@ const Finance = () => {
   };
 
   return (
-    <Box 
-      w="100vw"
-      h="100vh"
-      d="flex"
-      flexDir="column"
-      alignItems="center"
-    >
+    <Box w="100vw" h="100vh" d="flex" flexDir="column" alignItems="center">
       <Slide in={onContainerOpen} style={{ zIndex: 10 }} direction="left">
-        <Box
-        w="100%"
-        mb="10px"
-      >
-        <Header />
-        <Box
-          w="90%"
-          maxW="779.73px"
-          margin="0 auto"
-          h="77vh"
-          borderRadius="10px"
-          boxShadow="0px 5px 10px 1px rgba(0,0,0,0.5)"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          mt="20px"
-        >
+        <Box w="100%" mb="10px">
+          <Header />
           <Box
-          w="100%">
-            <HeaderPage 
-            onOpen={onAddFinanceOpen}
-            titulo="Lista de finanças"
-            />
-            <Box
-              w="100%"
-              d="flex"
-              flexDir={["column-reverse","","row"]}
-              justifyContent="space-evenly"
-              alignItems="center"
-            >
-              <Button variant="default" mt={["5px","","0px"]} w={["95%","70%","100px"]} onClick={() => setFilterFin("Todos")}>Todos</Button>
-              <Button variant="default" mt={["5px","","0px"]} w={["95%","70%","100px"]} onClick={() => setFilterFin("Entrada")}>Entradas</Button>
-              <Button variant="default" mt={["5px","","0px"]} w={["95%","70%","100px"]} onClick={() => setFilterFin("Despesa")}>Despesas</Button>
-              <TotalFinances d={newFinances.length > 0 ? "block" : "none"}finances={finances} />
-            </Box>
-
-          </Box>
-          <Box 
-            w={["95%","70%","90%"]}
-            borderRadius="4px"
-            h={["32vh","","50vh"]}
-            overflow="auto"
-            mt="10px"
+            w="90%"
+            maxW="779.73px"
+            margin="0 auto"
+            h="77vh"
+            borderRadius="10px"
+            boxShadow="0px 5px 10px 1px rgba(0,0,0,0.5)"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            mt="20px"
           >
-            {newFinances.length > 0 ?
-            newFinances.filter(({status}) =>(filterFin === "Todos")?status !== "Todos" : status === filterFin).map((item,index) => (
-              <FinanceCard item={item} key={index} />
-            )):
+            <Box w="100%">
+              <HeaderPage
+                onOpen={onAddFinanceOpen}
+                titulo="Lista de finanças"
+              />
               <Box
-                h="100%"
                 w="100%"
                 d="flex"
-                justifyContent="center"
+                flexDir={["column-reverse", "", "row"]}
+                justifyContent="space-evenly"
                 alignItems="center"
               >
-                <Heading 
-                  variant="title3" 
-                  fontSize={["16px","28px"]} 
+                <Button
+                  variant="default"
+                  mt={["5px", "", "0px"]}
+                  w={["95%", "70%", "100px"]}
+                  onClick={() => setFilterFin("Todos")}
+                >
+                  Todos
+                </Button>
+                <Button
+                  variant="default"
+                  mt={["5px", "", "0px"]}
+                  w={["95%", "70%", "100px"]}
+                  onClick={() => setFilterFin("Entrada")}
+                >
+                  Entradas
+                </Button>
+                <Button
+                  variant="default"
+                  mt={["5px", "", "0px"]}
+                  w={["95%", "70%", "100px"]}
+                  onClick={() => setFilterFin("Despesa")}
+                >
+                  Despesas
+                </Button>
+                <TotalFinances
+                  d={newFinances.length > 0 ? "block" : "none"}
+                  finances={finances}
+                />
+              </Box>
+            </Box>
+            <Box
+              w={["95%", "70%", "90%"]}
+              borderRadius="4px"
+              h={["32vh", "", "50vh"]}
+              overflow="auto"
+              mt="10px"
+              display="flex"
+              flexDirection="column"
+              gap="15px"
+            >
+              {newFinances.length > 0 ? (
+                newFinances
+                  .filter(({ status }) =>
+                    filterFin === "Todos"
+                      ? status !== "Todos"
+                      : status === filterFin
+                  )
+                  .map((item, index) => <FinanceCard item={item} key={index} />)
+              ) : (
+                <Box
+                  h="100%"
+                  w="100%"
                   d="flex"
-                  flexDir="column"
+                  justifyContent="center"
                   alignItems="center"
                 >
-                  sem despesas no momento
-                  <IoMdCash/>
-                </Heading>
-              </Box>
-            }
+                  <Heading
+                    variant="title3"
+                    fontSize={["16px", "28px"]}
+                    d="flex"
+                    flexDir="column"
+                    alignItems="center"
+                  >
+                    sem despesas no momento
+                    <IoMdCash />
+                  </Heading>
+                </Box>
+              )}
+            </Box>
+            <ModalFinance
+              isOpen={isAddFinanceOpen}
+              onClose={onAddFinanceClose}
+              handleChange={handleRegisterFinance}
+              title="Registrar finança"
+            />
           </Box>
-          <ModalFinance
-            isOpen={isAddFinanceOpen}
-            onClose={onAddFinanceClose}
-            handleChange={handleRegisterFinance}
-            title="Registrar finança"
-          />
-        </Box> 
-      </Box>
+        </Box>
       </Slide>
-      
-
     </Box>
   );
 };
